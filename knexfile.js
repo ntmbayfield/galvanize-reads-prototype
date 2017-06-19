c0b6d31ec3894bf81e066a0a921981cf881540a3
Justin Baize <justin.baize@gJBc897ad.local>
2017-06-19 15:44:54 -0500

    First real commit

---
+++ knexfile.js
@@ -0,0 +1,26 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection:{
+      host: "localhost",
+      user: "justin.baize",
+      password:"",
+      database: 'first_try'
+    }
+  },
+
+  // staging: {},
+
+  production: {
+    // client: 'postgresql',
+    // connection: {
+    //   database: 'my_db',
+    //   user:     'username',
+    //   password: 'password'
+    // },
+  }
+
+};
