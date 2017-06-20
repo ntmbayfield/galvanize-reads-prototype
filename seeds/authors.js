6cf4bba88cd3895c9a7e1c3984d66cb0a8e6c14d
Justin Baize <justin.baize@gJBc897ad.local>
2017-06-20 13:18:58 -0500

    Unnecessary commit

--- seeds/authors.js
+++ seeds/authors.js
@@ -2,21 +2,12 @@
 
 const authors = require('../entries/authorSeedEntries');
 
-// exports.seed = function(knex, Promise) {
-//   return knex('authors').del()
-//     .then(() =>
-//        knex('authors').insert(authors)
-//         .then(() =>
-//           knex.raw('SELECT setval(\'authors_id_seq\', (SELECT MAX(id) FROM authors))'));
-//         )
-//     };
-// };
-
 exports.seed = function (knex, Promise) {
-  // Deletes ALL existing entries
   return knex('authors').del()
-    .then(() =>
-       knex('authors').insert(authors))
-       .then(() => knex.raw(
-      'SELECT setval(\'authors_id_seq\', (SELECT MAX(id) FROM authors))'));
+    .then( () =>
+      knex('authors').insert(authors)
+    )
+    .then( () => knex.raw(
+      "SELECT setval('authors_id_seq', (SELECT MAX(id) FROM authors))")
+    );
 };
