a5ee1b8adb70dd8e96b542e9efefe6ca621e9966
Justin Baize <justin.baize@gJBc897ad.local>
2017-06-20 13:12:49 -0500

    Finished code cleanup

--- seeds/books_authors.js
+++ seeds/books_authors.js
@@ -2,22 +2,12 @@
 
 const booksAuthors = require('../entries/book-authorSeedEntries.js');
 
-// exports.seed = function(knex, Promise) {
-//   return knex('books_authors').del()
-//     .then(() =>
-//       knex('books_authors').insert(booksAuthors)
-//         .then( () =>
-//           knex.raw("SELECT setval('books_authors_id_seq',(SELECT MAX(id) FROM books_authors))"))
-//         })
-//     };
-// };
-
 exports.seed = function (knex, Promise) {
   return knex('books_authors').del()
     .then( () =>
-       knex('books_authors').insert(booksAuthors)
+      knex('books_authors').insert(booksAuthors)
     )
-   .then( () => knex.raw(
-      'SELECT setval(\'books_authors_id_seq\', (SELECT MAX(id) FROM books_authors))')
-   );
+    .then( () => knex.raw(
+      "SELECT setval('books_authors_id_seq', (SELECT MAX(id) FROM books_authors))")
+    );
 };
